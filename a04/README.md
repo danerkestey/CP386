99b8785c0daaf1aa850d0d0901ec7ac2de2f1fb0
Daner Yasin <77366878+danerkestey@users.noreply.github.com>
2021-07-26 14:04:22 -0400

    new init

---
+++ a04/README.md
@@ -0,0 +1,21 @@
+## Project title
+
+## Motivation
+
+## Installation
+
+## Features
+
+## Tests
+
+## Code Example
+
+## Screenshots
+
+## Individual Contribution (Function-wise)
+
+## Authors
+
+## Credits
+
+## License
--- a04/bankers.c
+++ a04/bankers.c
@@ -1,8 +1,9 @@
 /*
 --------------------------------------------------
-Project: CP386-assignment 2
+Project: CP386-assignment 4
 File:    bankers.c
-Author:  Daner Yasin   | 190773490
-Version: 2021-06-19
+Author:  Daner Yasin     | 190773490
+         Jacob Goldman   | 190773490
+Version: 2021-07-26
 --------------------------------------------------
 */
