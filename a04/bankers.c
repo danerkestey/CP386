565a98381be001c1eb9cf2ab5d6b5775d2196613
Daner Yasin <77366878+danerkestey@users.noreply.github.com>
2021-07-26 14:23:12 -0400

    updated

--- a04/bankers.c
+++ a04/bankers.c
@@ -3,7 +3,20 @@
 Project: CP386-assignment 4
 File:    bankers.c
 Author:  Daner Yasin     | 190773490
-         Jacob Goldman   | 190773490
+         Jacob Goldman   | 190509690
 Version: 2021-07-26
 --------------------------------------------------
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    
+
+
+
+}
