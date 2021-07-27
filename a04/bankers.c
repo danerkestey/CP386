e9d4b718c35d5d70aa2d9ba85bb5ab1ed32c5e6b
Daner Yasin <77366878+danerkestey@users.noreply.github.com>
2021-07-26 23:46:23 -0400

    notes for commands

--- a04/bankers.c
+++ a04/bankers.c
@@ -20,6 +20,20 @@ int request(int *available, int **max, int **allocation, int **need);
 int release(int *available, int **max, int **allocation, int **need);
 void status(int *available, int **max, int **allocation, int **need);
 
+/*
+    COMMANDS:
+        RQ  -   for requesting resources (remember threads cannot request more than maximum number of resource for that thread)
+                the command would fill the allocation array. the customer would  use  ‘RQ’  to  request  for  the  resources  for  all  the customers/thread
+                ex: RQ 0 3 1 2 1
+
+        RL  -   for releasing resources -> ex: RL 4 1 2 3 1
+
+        *   -   would  output  the current  stateof the available, maximum, allocation, and need arrays
+
+        Run -   find the safe sequence and run each thread
+                The command would execute the safe sequence based on the current state and all the threads would be run same function code and prints
+*/
+
 int main(int argc, char *argv[])
 {
     int available[argc - 1];
@@ -35,10 +49,12 @@ int exit(int *available, int **max, int **allocation, int **need)
 {
 }
 
+//  grant a request, if itdoes leave the system in a safe state, otherwisewill deny it.
 int safetyAlgorithm(int *available, int **max, int **allocation, int **need)
 {
 }
 
+//  return 0 if successful, 01 if unsuccessful
 int request(int *available, int **max, int **allocation, int **need)
 {
 }
